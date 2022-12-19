e19b935e2119a0da7fb8f379a3032d2ade56ff3f
Iacopo Buffoni <itsmadpupp@outlook.it>
2022-12-19 08:39:01 +0100

    Finish

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Inserisci il nome
+const _Name = prompt("Inserisci il tuo nome: ");
+
+// Inserisci il cognome
+const _LastName = prompt("Inserisci il tuo cognome: ");
+
+// Inserisci il colore
+const _Color = prompt("Inserisci il tuo colore preferito: ");
+
+// Mostra la password
+document.getElementById("Password").innerHTML = _Name + _LastName + _Color + "21";
